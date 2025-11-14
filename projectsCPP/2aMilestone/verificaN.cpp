185d5c8dbe6981aaa95dfb3844d4f240e174af89
devillaa <devillajoaopedro532@gmail.com>
2025-11-13 22:29:01 -0300

    2a milestone

---
+++ projectsCPP/2aMilestone/verificaN.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+
+
+
+    // VERIFICAR SE UM NUMERO E IMPAR OU PAR
+    // int n;
+    // cout << "Digite um número";
+    // cin >> n;
+
+    // if ( n % 2 == 0 ){
+    //     cout <<"O número é par";
+    // } else {
+    //     cout <<"O número é impar";
+    // }
+    
+
+    // CONTAR DE 1 A 10
+    // while( n <=10 ){
+    //     cout << n << endl;
+    //     n++;
+    // }
+
+
+    // SOMA DOIS NUMEROS
+    int a;
+    cout << "Digite o 1o Numero da soma";
+    cin >> a;
+
+    int b;
+    cout << "Digite o 2o Numero da soma";
+    cin >> b;
+    
+
+     cout << a <<"+" << b << "=" << a+b;
+ 
+
+
+
+    return 0;
+}
+
